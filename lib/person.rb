ebf9d4dbb2cdb17730e6c7f00d4a5807e81a95bc
Melissa Sperry <sperry.mel@gmail.com>
2020-02-22 07:31:33 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+  # Class body
+
+  # Instance Method Definition
+  def talk
+    puts "Hello World!"
+def walk
+  puts "The Person is walking"
+end
+  end
+end
+
+
+ada = Person.new
+person.talk
+person.walk
